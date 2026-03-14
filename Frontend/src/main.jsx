14a544f35e1783e889a69a7709bbe06aac0c4803
priyanshu agrawal <priyanshuagrawal303@gmail.com>
2026-03-14 13:54:56 +0530

    backend on vm

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/globals.css";
-
+//main
 ReactDOM.createRoot(document.getElementById("root")).render(
 
-  <App />
+    <App />
 
 );
+    
